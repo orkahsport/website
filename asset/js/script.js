f4e833b01f9659ce5454178a197a770a65165f11
Mario <mario.ferrali@fizfab.com>
2024-03-05 14:44:05 +0100

    version stable jour 2

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,13 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-        const typewriter = new Typewriter('.fade-in-out', {
-            loop: false,
-            delay: 75,
-        });
-    
-        typewriter
-            .pauseFor(1000)
-            .typeString('Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac lectus arcu. Nam elementum imperdiet neque eu sagittis.')
-            .pauseFor(1000)
-            .start();
-    });
-    
+const descriptionDynamic = document.getElementById('typewriter');
+
+let typewriter = new Typewriter(descriptionDynamic, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .typeString('écologique')
+  .pauseFor(1000)
+  .deleteChars(10)
+  .typeString('équitable')
+  .pauseFor(1000)
+  .deleteChars(9)
+  .typeString('recyclé')
+  .pauseFor(1000)
+  .deleteChars(7)
+  .start();
